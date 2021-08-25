dab6717316e01613a98c06ca116a16a71f2c0498
m1016m <m1016m@gmail.com>
2021-08-25 17:46:21 +0800

    Add code

--- ch08www/wsgi.py
+++ ch08www/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise #heroku add
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ch08www.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application) #heroku add
+
