7f40b2fd9abf65f61eb572dc53f7e6bdc9132dcc
“billyseel” <faizalre@yahoo.com.br>
2022-11-18 10:51:35 +0800

    make it better

--- mysite/forms.py
+++ mysite/forms.py
@@ -26,7 +26,7 @@ class PostForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
-        self.fields['mood'].label = '現在心情'
-        self.fields['nickname'].label = '你的暱稱'
-        self.fields['message'].label = '心情留言'
-        self.fields['del_pass'].label = '設定密碼'
+        self.fields['mood'].label = 'Achievement'
+        self.fields['nickname'].label = 'Nickname'
+        self.fields['message'].label = 'Message'
+        self.fields['del_pass'].label = 'delete code'
