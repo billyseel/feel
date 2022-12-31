3641732c4484c0d8bd18db48d91c97a9302337bc
“billyseel” <faizalre@yahoo.com.br>
2022-12-31 11:31:31 +0800

    make it better

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from mysite import models
- 
+
+
 class PostAdmin(admin.ModelAdmin):
-    list_display=('nickname', 'message', 'enabled', 'pub_time')
-    ordering=('-pub_time',)#倒著數，這樣才不會54321
+    list_display = ('nickname', 'message', 'enabled', 'pub_time')
+    ordering = ('-pub_time',)  # 倒著數，這樣才不會54321
+
+
 admin.site.register(models.Mood)
 admin.site.register(models.Post, PostAdmin)
