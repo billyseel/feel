8d98db613b0ef3261144a9be449be6ea31258e44
“billyseel” <faizalre@yahoo.com.br>
2023-01-03 10:46:48 +0800

    make it better

--- ch08www/urls.py
+++ ch08www/urls.py
@@ -20,11 +20,11 @@ from mysite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('<int:pid>/<str:del_pass>', views.index),
     path('list/', views.listing),
     path('post/', views.posting),
     path('contact/', views.contact),
     path('post2db/', views. post2db),
+    path('<int:pid>/<str:del_pass>', views.index),
 
 
 ]
